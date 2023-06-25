b6a6d91619189586ad3ec49d1fc435d811230f1c
Sardor Djurakulov <sarik.j2445@gmail.com>
2023-06-25 17:59:40 +0500

    added adaptivka

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,8 @@
 const dropZone = document.getElementById('dropZone');
+const image = document.querySelector('.bg1')
+const form = document.forms.sale
 
+let screenshoot;
 // Предотвращаем стандартное поведение перетаскивания (открытие файла в браузере)
 dropZone.addEventListener('dragover', (event) => {
   event.preventDefault();
@@ -18,5 +21,42 @@ dropZone.addEventListener('drop', (event) => {
 // Обработка загрузки файла
 function handleFileUpload(file) {
   // Здесь вы можете выполнить необходимые операции с загруженным файлом
-  console.log('Загруженный файл:', file.name);
+  dropZone.innerText = `Загруженный файл: ${file.name}`
+  
+  screenshoot = file
+  
+  dropZone.style.borderColor = "yellow"
 }
+
+
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  let data = {
+    screenshoot: screenshoot
+  }
+
+  if(!screenshoot){
+    dropZone.style.borderColor = "red"
+    return;
+  }
+
+  let fm = new FormData(form)
+
+  fm.forEach((value, key) => {
+    data[key] = value
+  })
+
+
+  console.log(data);
+}
+
+let num = 1
+
+setInterval(() => {
+  image.style.backgroundImage = `url(../images/bg${num}.png)`
+  num++
+  if(num > 3){
+    num = 1
+  }
+}, 3000)
